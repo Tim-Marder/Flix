da3344f778ac79f207cd5ce65653ac53fa770385
Tim-Marder <tmarder1@binghamton.edu>
2020-10-02 23:31:56 -0400

    completed unit 2 assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Tim Marder on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
